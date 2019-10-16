4905e343b972ed38ca569fb7be7b5612af9174b7
mikkop <mikkop@mikkops-MacBook-Air.local>
2019-10-16 21:53:40 +0300

    Add basic json server

--- main.go
+++ main.go
@@ -1,13 +1,22 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
+	"log"
+	"net/http"
 
 	"github.com/mikcompany/ravireader/veikkausapi"
 )
 
-func main() {
-	fmt.Println("Ravi Reader V0.1")
+func rootHandler(w http.ResponseWriter, req *http.Request) {
+	io.WriteString(w, "ravireader")
+}
+
+func jsonHandler(w http.ResponseWriter, req *http.Request) {
+
+	var result []interface{}
 
 	cards := veikkausapi.FetchCards()
 
@@ -18,9 +27,25 @@ func main() {
 			races := veikkausapi.FetchRaces(card.CardID)
 
 			for _, race := range races {
-				fmt.Println(veikkausapi.FetchResult(race.RaceID))
+				//fmt.Println(veikkausapi.FetchResult(race.RaceID))
+				result = append(result, veikkausapi.FetchResult(race.RaceID))
 			}
 		}
 	}
 
+	jData, err := json.Marshal(result)
+	if err != nil {
+		// handle error
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(jData)
+}
+
+func main() {
+	fmt.Println("Ravi Reader V0.1")
+
+	http.HandleFunc("/", rootHandler)
+	http.HandleFunc("/json", jsonHandler)
+	log.Fatal(http.ListenAndServe(":80", nil))
 }
